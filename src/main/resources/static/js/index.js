048f17833ba35424149cb7271c12a39cf117c5bf
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-04-07 14:06:45 -0300

    move logic to user.js

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -12,26 +12,6 @@ window.addEventListener('load', function () {
   const jwt = localStorage.getItem('jwt')
 
   const nodoNombreUsuario = document.querySelector('.user-info a small')
-  const btnCerrar = document.querySelector('#closeApp')
-
-  btnCerrar.addEventListener('click', function () {
-    Swal.fire({
-      title: '¿Desea cerrar sesión?',
-      text: 'Para ingresar nuevamente tendrá que introducir sus credenciales.',
-      icon: 'question',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: '¡Si!',
-      cancelButtonText: '¡Mejor no!'
-    }).then(result => {
-      if (result.isConfirmed) {
-        //  cerrar sesion del usuario
-        localStorage.clear()
-        location.href = '/login.html'
-      }
-    })
-  })
 
   /* --------------- funciones que se disparan al iniciar la app -------------- */
 
@@ -39,8 +19,6 @@ window.addEventListener('load', function () {
 
   if (!user) {
     obtenerNombreUsuario('http://localhost:8080/user', jwt)
-  } else {
-    nodoNombreUsuario.innerHTML = user
   }
 
   /* ---------------------- GET: obtener info del usuario --------------------- */
